8e96f7689e4de53797fadeed7341a5cc1c6c794a
Chedy Chaaben <chedychaaben@gmail.com>
2025-04-22 03:15:08 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,27 +46,33 @@ const Header = () => {
 
     fetchProfileImage();
   }, []);
-
+  const navLinkClass = "flex items-center gap-2 mr-5 hover:text-primary";
+  const isAdmin = user?.isAdmin;
+  const isLoggedIn = !!user;
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background mx-auto flex p-3 lg:px-16 items-center justify-end">
       <NavLink to="/" className="inline-flex -order-1 items-center gap-2">
         <img src={logo} width={38} alt="RideShare" />
         <h1 className="font-bold text-xl text-primary hidden sm:block">FiTheniti</h1>
       </NavLink>
+
       <nav className="ml-auto flex items-center text-base justify-center">
-        {user?.isAdmin ?
-          <NavLink to="/admin/ListUsers" className="flex items-center gap-2 mr-5 hover:text-primary">
+        {isAdmin && (
+          <NavLink to="/admin/ListUsers" className={navLinkClass}>
             <Settings className="h-4 w-4" />
             Administration
           </NavLink>
-          :
-          ""
-          }
-        <NavLink to="/search" className="flex items-center gap-2 mr-5 hover:text-primary"><Search className="h-4 w-4" />Search</NavLink>
-        {!user ? (
+        )}
+
+        <NavLink to="/search" className={navLinkClass}>
+          <Search className="h-4 w-4" />
+          Search
+        </NavLink>
+
+        {!isLoggedIn ? (
           <Dialog>
             <DialogTrigger asChild>
-              <NavLink to="#" className="flex items-center gap-2 mr-5 hover:text-primary">
+              <NavLink to="#" className={navLinkClass}>
                 <PlusCircle className="h-4 w-4" />
                 Publish a ride
               </NavLink>
@@ -74,52 +80,68 @@ const Header = () => {
             <LoginSignupDialog />
           </Dialog>
         ) : (
-          <NavLink to="/offer-seat" className="flex items-center gap-2 mr-5 hover:text-primary">
-            <PlusCircle className="h-4 w-4" />
-            Publish a ride
-          </NavLink>
+          !isAdmin && (
+            <NavLink to="/offer-seat" className={navLinkClass}>
+              <PlusCircle className="h-4 w-4" />
+              Publish a ride
+            </NavLink>
+          )
         )}
       </nav>
-      {!user ?
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button>Login</Button>
-            </DialogTrigger>
-            <LoginSignupDialog />
-          </Dialog>
-        :
+
+      {!isLoggedIn ? (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Login</Button>
+          </DialogTrigger>
+          <LoginSignupDialog />
+        </Dialog>
+      ) : (
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar>
               <AvatarImage src={`${backendUri}/uploads/${profileImage}`} />
-              <AvatarFallback className="select-none text-primary text-xl font-bold">{user.user?.name[0]}</AvatarFallback>
+              <AvatarFallback className="select-none text-primary text-xl font-bold">
+                {user.user?.name[0]}
+              </AvatarFallback>
             </Avatar>
           </DropdownMenuTrigger>
+
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
               <Link to="/profile" className="flex">
-                <User className="mr-2 h-4 w-4"/><span>Profile</span>
+                <User className="mr-2 h-4 w-4" />
+                <span>Profile</span>
               </Link>
             </DropdownMenuItem>
-            
-            <DropdownMenuItem>
-              <Link to="/published-rides" className="flex">
-              <Car className="mr-2 h-4 w-4"/><span>Published Rides</span>
-              </Link>
+
+            {!isAdmin && (
+              <>
+                <DropdownMenuItem>
+                  <Link to="/published-rides" className="flex">
+                    <Car className="mr-2 h-4 w-4" />
+                    <span>Published Rides</span>
+                  </Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <Link to="/booked-rides" className="flex">
+                    <History className="mr-2 h-4 w-4" />
+                    <span>Booked Rides</span>
+                  </Link>
+                </DropdownMenuItem>
+              </>
+            )}
+
+            <DropdownMenuItem className="cursor-pointer" onClick={handleLogout}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Log Out</span>
             </DropdownMenuItem>
-            
-            <DropdownMenuItem>
-              <Link to="/booked-rides" className="flex">
-                <History className="mr-2 h-4 w-4"/><span>Booked Rides</span>
-              </Link>
-            </DropdownMenuItem>
-            
-            <DropdownMenuItem className='cursor-pointer' onClick={handleLogout}><LogOut className="mr-2 h-4 w-4"/><span>Log Out</span></DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      }
+      )}
     </header>
   )
 }
